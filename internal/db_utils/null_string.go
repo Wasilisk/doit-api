817fb7f70dcf2c81a216f41b3fd71c87a0734a40
Wasilisk <vasylpetryna2002@gmail.com>
2026-03-03 20:03:59 +0000

    feat: add utility function for handling nullable strings in database queries

---
+++ internal/db_utils/null_string.go
@@ -0,0 +1,10 @@
+package dbutils
+
+import "database/sql"
+
+func NullString(s *string) sql.NullString {
+	if s == nil {
+		return sql.NullString{}
+	}
+	return sql.NullString{String: *s, Valid: true}
+}
