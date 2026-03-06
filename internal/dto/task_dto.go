e6ea36b7b840cd89481ce8c5f2d044200858a392
Wasilisk <vasylpetryna2002@gmail.com>
2026-03-06 17:02:04 +0000

    feat: add DTOs for task creation, update, and filtering

---
+++ internal/dto/task_dto.go
@@ -0,0 +1,43 @@
+package dto
+
+type CreateTaskRequest struct {
+	Name        string   `json:"name"        binding:"required"`
+	Description *string  `json:"description"`
+	Date        *int64   `json:"date"`
+	TimeStart   *int64   `json:"time_start"`
+	TimeEnd     *int64   `json:"time_end"`
+	TagIDs      []string `json:"tag_ids"`
+}
+
+type UpdateTaskRequest struct {
+	Name        *string  `json:"name"`
+	Description *string  `json:"description"`
+	Date        *int64   `json:"date"`
+	TimeStart   *int64   `json:"time_start"`
+	TimeEnd     *int64   `json:"time_end"`
+	TagIDs      []string `json:"tag_ids"`
+	IsCompleted *bool    `json:"is_completed"`
+	IsFavourite *bool    `json:"is_favourite"`
+}
+
+type TaskFilterRequest struct {
+	Date        *int64  `form:"date"`
+	TagID       *string `form:"tag_id"`
+	IsCompleted *bool   `form:"is_completed"`
+	IsDeleted   *bool   `form:"is_deleted"`
+}
+
+type TaskResponse struct {
+	ID          string        `json:"id"`
+	UserID      string        `json:"user_id"`
+	Name        string        `json:"name"`
+	Description *string       `json:"description"`
+	Date        *int64        `json:"date"`
+	TimeStart   *int64        `json:"time_start"`
+	TimeEnd     *int64        `json:"time_end"`
+	IsCompleted bool          `json:"is_completed"`
+	IsFavourite bool          `json:"is_favourite"`
+	DeletedAt   *string       `json:"deleted_at"`
+	Tags        []TagResponse `json:"tags"`
+	CreatedAt   string        `json:"created_at"`
+}
